18494ea92a940ba18cab24ce02502892f974f5a6
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-06-12 09:00:39 +0200

    pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Quiz from './components/Quiz';
 import './App.css';
